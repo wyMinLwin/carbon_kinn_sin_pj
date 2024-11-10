3bd8007aa9d4aba481a700a0c14509e025e74c8f
Htet Arkar <115476708+htetarkar2246@users.noreply.github.com>
2024-11-10 21:38:21 +0630

    Update admin.py

--- carbon_kinn_sin/stickers/admin.py
+++ carbon_kinn_sin/stickers/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import StickerType, Sticker
 
 class StickerTypeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name',)
+    list_display = ('name',)
     search_fields = ('name',)
     list_filter = ('name',)
 
 class StickerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'sticker_type', 'lat', 'lng','image')
+    list_display = ('sticker_type', 'lat', 'lng','image')
     search_fields = ('sticker_type__name',)
     list_filter = ('sticker_type',)
 
