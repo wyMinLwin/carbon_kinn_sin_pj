6f94f076370a52c9b110926ba2c4124b4489ced8
Htet Arkar <115476708+htetarkar2246@users.noreply.github.com>
2024-11-10 17:58:02 +0630

    Update settings.py

--- carbon_kinn_sin/carbon_kinn_sin/settings.py
+++ carbon_kinn_sin/carbon_kinn_sin/settings.py
@@ -128,7 +128,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #meida 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
