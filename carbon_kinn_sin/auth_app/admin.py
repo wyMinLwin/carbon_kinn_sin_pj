81eb7bdb7cef8931f3903f55b45fb49982f2d8df
Htet Arkar <115476708+htetarkar2246@users.noreply.github.com>
2024-11-10 18:25:42 +0630

    Update admin.py

--- carbon_kinn_sin/auth_app/admin.py
+++ carbon_kinn_sin/auth_app/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
 class UserAdmin(BaseUserAdmin):
-    list_display = ('email', 'name', 'ph_num', 'is_staff', 'is_active')
+    list_display = ('email', 'name', 'ph_num', 'is_staff', 'is_active', 'collected_stickers')
     search_fields = ('email', 'name', 'ph_num')
     list_filter = ('is_staff', 'is_active')
 
@@ -16,14 +16,21 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'name', 'ph_num', 'password1', 'password2', 'is_active', 'is_staff')}
+            'fields': ('email', 'name', 'ph_num', 'password1', 'password2', 'is_active', 'is_staff')},
         ),
     )
 
     ordering = ('email',)
-    
+
     def has_add_permission(self, request):
         return False
+
+    def collected_stickers(self, obj):
+        """Display collected stickers in admin as a list of IDs or names (based on your model setup)."""
+        stickers = obj.collected_stickers()
+        return ", ".join([str(sticker) for sticker in stickers]) if stickers else "No stickers"
+
+    collected_stickers.short_description = 'Collected Stickers'
 
 
 admin.site.register(User, UserAdmin)
