4ba6df5c029ecc254e52d55d698ee73bd2bedc9b
Htet Arkar <115476708+htetarkar2246@users.noreply.github.com>
2024-11-10 21:32:14 +0630

    Update models.py

--- carbon_kinn_sin/stickers/models.py
+++ carbon_kinn_sin/stickers/models.py
@@ -10,8 +10,8 @@ class Sticker(models.Model):
     code = models.CharField(max_length=20, unique=True)  
     sticker_type = models.ForeignKey(StickerType, related_name='stickers', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='core/media')  
-    lat = models.FloatField(blank=True)  # Latitude for geographical location
-    lng = models.FloatField(blank=True)  # Longitude for geographical location
+    lat = models.FloatField(null=True, blank=True)  # Latitude for geographical location
+    lng = models.FloatField(null=True, blank=True)  # Longitude for geographical location
     special = models.BooleanField(default=False)
 
     def __str__(self):
